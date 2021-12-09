d93fee30b43436da206e1368601e2f2455516387
rx1310 <rx1310@inbox.ru>
2021-12-09 14:35:34 +0300

    Небольшие правки

--- src/assets/js/themer.js
+++ src/assets/js/themer.js
@@ -1,6 +1,8 @@
 // ! Copyright (c) 2021, Haba Kudzaev <rx1310@inbox.ru>
 
 const storageKey = "k19-theme";
+
+
 const themes = {
 	light: "theme-light",
 	dark: "theme-dark"
@@ -9,18 +11,23 @@ const themes = {
 /* themerSet()
  * └── меняет значение в localStorage
  * @param theme - указывается название темы из списка themes[] */
-function themerSet(theme) {
+function themerSet(theme, color) {
 	localStorage.setItem(storageKey, theme);
 	document.documentElement.className = theme;
+	if (color) {
+		setColorTheme(color);
+	} else {
+		setColorTheme("#ffffff")
+	}
 }
 
 /* themerToggle()
  * └── переключает тему со светлой на темную и наоборот */
 function themerToggle() {
 	if (localStorage.getItem(storageKey) === themes.dark) {
-		themerSet(themes.light);
+		themerSet(themes.light, "#ffffff");
 	} else {
-		themerSet(themes.dark);
+		themerSet(themes.dark, "#050505");
 	}
 }
 
@@ -28,8 +35,13 @@ function themerToggle() {
  * └── применяет тему из localStorage при загрузке страницы */
 function themerApply() {
 	if (localStorage.getItem(storageKey) === themes.dark) {
-		themerSet(themes.dark);
+		themerSet(themes.dark, "#050505");
 	} else {
-		themerSet(themes.light);
+		themerSet(themes.light, "#fffff");
 	}
 } themerApply();
+
+function setColorTheme(color) {
+	let scheme = document.querySelector('meta[name="theme-color"]');
+	scheme.setAttribute('content', color);
+}
