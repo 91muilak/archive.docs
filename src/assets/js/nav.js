317e1563bea60fe7e2dbafbbf5daaf2389e9c16a
rx1310 <rx1310@inbox.ru>
2021-12-14 03:34:10 +0300

    Реорганизация проекта

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', (e) => {
 
 	const nav = document.querySelector('.nav');
 
-	if (window.pageYOffset > 50) {
+	if (window.pageYOffset > 55) {
 		nav.classList.add("nav--shadow");
 	} else {
 		nav.classList.remove("nav--shadow");
